02b3524b88792b6fd60259a917db898da456981a
Đorđe Matić <djmatic@agfbl.org>
2022-08-13 21:02:04 +0200

    ready for deploy-final

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const app = express();
 
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb+srv://studentList:efibdVyWdBTr1QSV@studentsdb.mzswlbw.mongodb.net/?retryWrites=true&w=majority"
-);
+mongoose.connect(process.env.DATABASE_URL);
 
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
