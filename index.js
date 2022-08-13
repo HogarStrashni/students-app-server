025ca6e0984a6b7da6891c84173c60f57836d9bc
Đorđe Matić <djmatic@agfbl.org>
2022-08-13 18:14:34 +0200

    ready for deploy

--- index.js
+++ index.js
@@ -17,4 +17,4 @@ app.use(express.json());
 const studentsRouter = require("./routes/students");
 app.use("/api", studentsRouter);
 
-app.listen(3000, () => console.log("Server Started"));
+app.listen(process.env.PORT || 3000, () => console.log("Server Started"));
